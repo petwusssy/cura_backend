a86f0bb2a6749e46460f29a26fdb2a144c060322
petwusssy <ocampobaltazar48@gmail.com>
2026-08-02 22:34:54 +0800

    Use custom python script for creating superuser via DJANGO_USERNAME

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py createsuperuser --noinput || true
+python -c "import os, django; os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings'); django.setup(); from django.contrib.auth import get_user_model; User = get_user_model(); u = os.environ.get('DJANGO_USERNAME'); p = os.environ.get('DJANGO_PASSWORD'); User.objects.filter(username=u).exists() or (u and p and User.objects.create_superuser(username=u, email='', password=p))" || true
